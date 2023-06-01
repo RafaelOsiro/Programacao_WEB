e2a30d5c745380d63f7e9c6174a3def7200c98f7
Rafael Riki Ogawa Osiro <rafaelosiro@hotmail.com>
2023-06-01 01:31:39 -0300

    Feat: Increase more attributes to register and edit person

--- src/controllers/pessoaController.js
+++ src/controllers/pessoaController.js
@@ -8,7 +8,11 @@ function cadastrarPessoa(req, res){
     let pessoa = {
         nome: req.body.nome,
         sobrenome: req.body.sobrenome,
-        cpf: req.body.cpf
+        cpf: req.body.cpf,
+        email: req.body.email,
+        telefone: req.body.telefone,
+        altura: req.body.altura,
+        peso: req.body.peso
     }
     
     Pessoa.create(pessoa).then((result)=>{
@@ -34,7 +38,7 @@ function editarView(req, res){
     let id = req.params.id
     let pessoa;
     Pessoa.findByPk(id).then(function(pessoa){
-        res.render("pessoa/editar.html", {pessoa});
+        res.render("pessoa/editar.html", {pessoa, "pessoaId": req.params.id});
     })
 }
 
@@ -43,21 +47,43 @@ function editarPessoa(req, res) {
         nome: req.body.nome,
         sobrenome: req.body.sobrenome,
         cpf: req.body.cpf,
+        email: req.body.email,
+        telefone: req.body.telefone,
+        altura: req.body.altura,
+        peso: req.body.peso
     }
     Pessoa.update(
-      pessoa,
-      {
+        pessoa,
+        {
         where: {
-          id: req.body.id,
+            id: req.body.id,
         },
-      }
+        }
     ).then(function (sucesso) {
         res.render("pessoa/editar.html", {pessoa, sucesso});
     })
     .catch(function (erro) {
         res.render("pessoa/editar.html", {pessoa, erro})
     });
+}
 
+function deletarPessoa(req, res) {
+    
+    Pessoa.destroy({
+        where: {
+            id: req.body.pessoaId
+        }
+    }).then(numDeleted => {
+        Pessoa.findAll().then((pessoas) => {
+            res.render("pessoa/listar.html", {pessoas});
+        }).catch((err) => {
+            console.log("Erro ao listar os usuários. ", err);
+            let erro = err
+            res.render("pessoa/listar.html", {erro});
+        })
+    }).catch(err => {
+        console.log("Erro ao excluir o usuário. ", err);
+    })
 }
 
 module.exports =  {
@@ -66,4 +92,5 @@ module.exports =  {
     listarView,
     editarView,
     editarPessoa,
+    deletarPessoa
 };
