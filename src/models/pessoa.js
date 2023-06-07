136230de621bef75984c659f949151428c18c5b9
Rafael Riki Ogawa Osiro <rafaelosiro@hotmail.com>
2023-06-07 11:23:23 -0300

    Feat: Created pessoa model

--- src/models/pessoa.js
+++ src/models/pessoa.js
@@ -1,38 +1,44 @@
 const Sequelize = require('sequelize');
 const database = require('../db');
- 
+
 const Pessoa = database.define('pessoa', {
-    id: {
-        type: Sequelize.INTEGER,
-        autoIncrement: true,
-        allowNull: false,
-        primaryKey: true
-    },
-    nome: {
-        type: Sequelize.STRING,
-        allowNull: false
-    },
-    sobrenome: {
-        type: Sequelize.STRING,
-        allowNull: false
-    },
-    cpf: {
-        type: Sequelize.STRING,
-        allowNull: false,
-        unique: true
-    },
-    email: {
-        type: Sequelize.STRING
-    },
-    telefone: {
-        type: Sequelize.INTEGER
-    },
-    altura: {
-        type: Sequelize.DOUBLE
-    },
-    peso: {
-        type: Sequelize.DOUBLE
-    },
+  id: {
+    type: Sequelize.BIGINT,
+    allowNull: false,
+    autoIncrement: true,
+    primaryKey: true,
+    unique: true,
+  },
+  nome: {
+    type: Sequelize.STRING(128),
+    allowNull: false
+  },
+  cpf: {
+    type: Sequelize.BIGINT,
+    allowNull: false,
+    unique: true
+  },
+  data_nascimento: {
+    type: Sequelize.DATE,
+    allowNull: false
+  },
+  telefone: {
+    type: Sequelize.BIGINT,
+    allowNull: false
+  },
+  email: {
+    type: Sequelize.STRING(32),
+    allowNull: false,
+    unique: true
+  },
+  endereco: {
+    type: Sequelize.STRING(255),
+    allowNull: false
+  },
+  cep: {
+    type: Sequelize.BIGINT,
+    allowNull: false
+  }
 })
- 
+
 module.exports = Pessoa;
