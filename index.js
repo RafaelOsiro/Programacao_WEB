fc3a21a4e78e8f0e926fd389e4d8473b0086dc23
Rafael Riki Ogawa Osiro <rafaelosiro@hotmail.com>
2023-06-08 23:32:06 -0300

    Fix: Add semicolon on lines

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
-const express = require('express')
-const mustacheExpress = require('mustache-express')
-const db = require('./src/db')
+const express = require('express');
+const mustacheExpress = require('mustache-express');
+const db = require('./src/db');
+const Pessoa = require('./src/models/pessoaModel');
+const Usuario = require('./src/models/usuarioModel');
+const Movimento = require('./src/models/movimentoModel');
+const ContaCorrente = require('./src/models/contaCorrenteModel');
 
 const app = express()
 
-app.engine('html', mustacheExpress())
-app.set('view engine', 'html')
-app.set('views', __dirname + '/src/views')
+app.engine('html', mustacheExpress());
+app.set('view engine', 'html');
+app.set('views', __dirname + '/src/views');
 
-app.use(express.urlencoded({extended: true}))
+app.use(express.urlencoded({ extended: true }));
 
 // Define as rotas da aplicação (declaradas na pasta /src/routes/)
 app.use('/', require('./src/routes/pessoaRoutes'));
@@ -18,5 +22,20 @@ db.sync(() => console.log(`Banco de dados conectado`));
 
 const app_port = 8000
 app.listen(app_port, function () {
-    console.log('app rodando na porta ' + app_port)
+    console.log('app rodando na porta ' + app_port);
 })
+
+Pessoa.findByPk(1)
+    .then((pessoa) => {
+        if (pessoa) {
+            usuarioBuild.setPessoa(pessoa);
+            console.log("PESSOA =>", pessoa);
+            return usuarioBuild.save();
+        }
+    })
+    .then(() => {
+        console.log("Usuário salvo com sucesso!");
+    })
+    .catch((error) => {
+        console.error("Erro ao salvar o usuário:", error);
+    });
