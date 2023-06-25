b1b88de06ff10ebb2bfbbcc7966039e17d43edee
Rafael Riki Ogawa Osiro <rafaelosiro@hotmail.com>
2023-06-25 20:33:48 -0300

    Chore: Removido codigos de debug

--- src/services/autenticador.js
+++ src/services/autenticador.js
@@ -1,14 +1,10 @@
 module.exports = {
   autenticar() {
     return (req, res, next) => {
-      console.log('auticador');
-      // console.log(req.session);
       if (req.session && req.session.autenticado) {
-        // O usuário está autenticado, permita o acesso à rota
         req.session.timestamp = Date.now();
         next();
       } else {
-        // O usuário não está autenticado, redirecione para a página de login
         res.redirect('/login');
       }
     };
