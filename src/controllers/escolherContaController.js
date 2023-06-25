7f4460b282c7057457c62e39ef33ccbc674eeb8c
Rafael Riki Ogawa Osiro <rafaelosiro@hotmail.com>
2023-06-25 20:34:29 -0300

    Feat: Mostrar no Select as opcoes de conta corrente do usuario

--- src/controllers/escolherContaController.js
+++ src/controllers/escolherContaController.js
@@ -1,10 +1,27 @@
-function escolherContaView(req, res) {
-  let pessoa = req.session.pessoa.nome;
+const ContaCorrente = require('../models/contaCorrenteModel');
 
-  res.render("escolherConta/escolherConta.html", { pessoa });
+async function escolherContaView(req, res) {
+  try {
+    const pessoaNome = req.session.pessoa.nome;
+    const usuarioId = req.session.usuario.id;
+
+    const contasCorrentes = await ContaCorrente.findAll({
+      where: { usuario_id: usuarioId },
+    });
+
+    let opcoes = '';
+
+    if (contasCorrentes.length > 0) {
+      opcoes = contasCorrentes.map(conta => `<option value="${conta.id}">${conta.nome}</option>`).join('');
+    }
+
+    res.render('escolherConta/escolherConta.html', { pessoa: pessoaNome, options: opcoes });
+  } catch (error) {
+    console.error('Erro ao exibir a página de escolher conta:', error);
+    const mensagem = 'Ocorreu um erro ao exibir a página de escolher conta.';
+    res.render('escolherConta/escolherConta.html', { mensagem });
+  }
 }
-
-
 
 module.exports = {
   escolherContaView
