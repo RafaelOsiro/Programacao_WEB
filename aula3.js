1876b9893ce4fbafc156d60faad91bbd6b181405
Rafael Riki Ogawa Osiro <rafaelosiro@hotmail.com>
2023-07-09 20:08:57 -0300

    Feat: Add aula3.js file

---
+++ aula3.js
@@ -0,0 +1,12 @@
+const calculadora = {
+    somar: function(n1, n2) {
+        return n1 + n2;
+    },
+
+    subtrair: function(n1, n2) {
+        return n1 - n2;
+    },
+}
+
+console.log(calculadora.somar(1, 1));
+console.log(calculadora.subtrair(1, 1));
